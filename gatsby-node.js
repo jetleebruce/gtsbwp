402f81e0a78526d35e4b6f01b7153436927259de
Serge Modin <modinsv@yandex.ru>
2021-04-20 20:13:35 +0300

    createPages for every post

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,34 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+    query {
+      allWpPost {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allWpPost.edges.forEach(edge => {
+    createPage({
+      // will be the url for the page
+      path: `/post/${edge.node.slug}`,
+      // specify the component template of your choice
+      component: path.resolve(`src/templates/posts.js`),
+      // In the ^template's GraphQL query, 'id' will be available
+      // as a GraphQL variable to query for this posts's data.
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+}
