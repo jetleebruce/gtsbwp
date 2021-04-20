e3895e91208179396d84ba4983cddfb6244bb70d
Serge Modin <modinsv@yandex.ru>
2021-04-20 23:09:22 +0300

    currentPostQuery set up

--- src/templates/posts.js
+++ src/templates/posts.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react"
+import { graphql } from "gatsby"
 
-const Posts = () => {
-    return (
-        <div>
-            <h2>Post Template</h2>
-        </div>
-    )
+import Layout from "../components/layout"
+
+export const PageQuery = graphql`
+  query currentPostQuery($id: String) {
+    wpPost(id: { eq: $id }) {
+      excerpt
+      title
+      id
+    }
+  }
+`
+
+const Posts = ({ data }) => {
+  const post = data.wpPost
+  console.log(post)
+  return (
+    <Layout>
+      <div>
+        <h2>Post Template</h2>
+        <p>{post.title}</p>
+      </div>
+    </Layout>
+  )
 }
 
 export default Posts
